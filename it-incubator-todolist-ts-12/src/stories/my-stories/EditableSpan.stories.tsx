f26a5866e798a3717d204d510d36742e56083b53
annavilnid <anna.vilnid@gmail.com>
2023-05-17 22:00:33 +0200

    12 main

---
+++ it-incubator-todolist-ts-12/src/stories/my-stories/EditableSpan.stories.tsx
@@ -0,0 +1,34 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {action} from '@storybook/addon-actions'
+import { EditableSpan } from '../../EditableSpan';
+
+// More on how to set up stories at:
+// https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof EditableSpan> = {
+  title: 'TODOLISTS/EditableSpan',
+  component: EditableSpan,
+  // This component will have an automatically generated Autodocs entry:
+  // https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    value: {
+      description: 'Start value empty sstring. Set start value'
+    },
+    onChange: {
+      description: 'set new value'
+    }
+  },
+  args: {
+    onChange: action('Change value editable span'),
+    value: 'HTML'
+  }
+};
+
+
+export default meta;
+type Story = StoryObj<typeof EditableSpan>;
+
+// More on component templates:
+// https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export const EditableSpanStory: Story = {};
